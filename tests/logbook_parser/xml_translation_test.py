deed53bfb9b1460036b2c504339bd23d3e7515e4
Chad Lowe <pfmsoft.dev@gmail.com>
2022-10-13 17:52:30 -0700

    fix imports

--- tests/logbook_parser/xml_translation_test.py
+++ tests/logbook_parser/xml_translation_test.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 from logbook_parser.xml_translation import (
-    dataclass_to_csv,
     flatten_flights,
     parse_XML,
 )
+from logbook_parser.util.dataclasses_to_csv import dataclasses_to_csv
 from logbook_parser.models.xml_element_model import LogbookElement
 
 
@@ -19,7 +19,8 @@ def test_write_flat_to_csv(report_data_ctx, test_app_data_dir: Path):
         data = parse_XML(file_path, {})
     flat_data = flatten_flights(data)
     file_out = test_app_data_dir / "csv_out.csv"
-    dataclass_to_csv(file_out, flat_data)
+    dataclasses_to_csv(dataclasses=flat_data, output_path=file_out)
+    assert False
 
 
 def test_flatten_flights(report_data_ctx):
@@ -29,21 +30,22 @@ def test_flatten_flights(report_data_ctx):
     assert len(flat_data) > 100
 
 
-def test_dataclass_to_csv(report_data_ctx, test_app_data_dir: Path):
-    with report_data_ctx as file_path:
-        data = parse_XML(file_path, {})
-    flat_data = flatten_flights(data)
-    file_out = test_app_data_dir / "csv_out.csv"
-    skips = "year_uuid"
-    dataclass_to_csv(file_out, flat_data, skips)
-    # assert False
+# TODO implement and test custom ordered fields
+# def test_dataclass_to_csv(report_data_ctx, test_app_data_dir: Path):
+#     with report_data_ctx as file_path:
+#         data = parse_XML(file_path, {})
+#     flat_data = flatten_flights(data)
+#     file_out = test_app_data_dir / "csv_out.csv"
+#     skips = "year_uuid"
+#     dataclasses_to_csv(dataclasses=flat_data, output_path=file_out)
+#     # assert False
 
 
-def test_dataclass_to_csv_skip_uuid(report_data_ctx, test_app_data_dir: Path):
-    with report_data_ctx as file_path:
-        data = parse_XML(file_path, {})
-    flat_data = flatten_flights(data)
-    file_out = test_app_data_dir / "csv_out_no_uuid.csv"
-    skips = "year_uuid month_uuid trip_uuid duty_period_uuid flight_uuid"
-    dataclass_to_csv(file_out, flat_data, skips)
-    # assert False
+# def test_dataclass_to_csv_skip_uuid(report_data_ctx, test_app_data_dir: Path):
+#     with report_data_ctx as file_path:
+#         data = parse_XML(file_path, {})
+#     flat_data = flatten_flights(data)
+#     file_out = test_app_data_dir / "csv_out_no_uuid.csv"
+#     skips = "year_uuid month_uuid trip_uuid duty_period_uuid flight_uuid"
+#     dataclass_to_csv(file_out, flat_data, skips)
+#     # assert False
